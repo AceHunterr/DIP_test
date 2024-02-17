5e7024cde78f1d92475622cb4340b1d3dc983882
Dhruvv245 <123222549+Dhruvv245@users.noreply.github.com>
2024-02-17 18:48:24 +0530

    Frontend Modified

--- DIP-Mehul/Digital_Image_Processing/sketch2.js
+++ DIP-Mehul/Digital_Image_Processing/sketch2.js
@@ -3,7 +3,6 @@
 // https://thecodingtrain.com/CodingChallenges/130-fourier-transform-drawing.html
 // https://youtu.be/MY4luNgGfms
 
-
 let x = [];
 let y = [];
 let fourierX;
@@ -12,7 +11,7 @@ let time = 0;
 let path = [];
 
 function setup() {
-  createCanvas(1100, 800);
+  createCanvas(1600, 800);
   const skip = 5;
   for (let i = 0; i < ramanujan.length; i += skip) {
     x.push(ramanujan[i].x);
@@ -20,10 +19,9 @@ function setup() {
   }
   fourierX = dft(x);
   fourierY = dft(y);
-  
+
   fourierX.sort((a, b) => b.amp - a.amp);
   fourierY.sort((a, b) => b.amp - a.amp);
-  
 }
 
 function epiCycles(x, y, rotation, fourier) {
@@ -35,7 +33,7 @@ function epiCycles(x, y, rotation, fourier) {
     let phase = fourier[i].phase;
     x += radius * cos(freq * time + phase + rotation);
     y += radius * sin(freq * time + phase + rotation);
-    
+
     stroke(255, 100);
     noFill();
     ellipse(prevx, prevy, radius * 2);
@@ -52,10 +50,10 @@ function draw() {
   let vy = epiCycles(100, height / 2 + 100, HALF_PI, fourierY);
   let v = createVector(vx.x, vy.y);
   path.unshift(v);
-  
-  line(vx.x, vx.y, v.x, v.y );
+
+  line(vx.x, vx.y, v.x, v.y);
   line(vy.x, vy.y, v.x, v.y);
-  
+
   beginShape();
   noFill();
   for (let i = 0; i < path.length; i++) {
@@ -70,8 +68,4 @@ function draw() {
   //   time = 0;
   //   // path = [];
   // }
-
 }
-
-
-
